7d142507c87758517ef05d0ad2eb5bb1975c408b
Mauro Presso <mauro.presso@gmail.com>
2025-08-16 19:27:36 -0300

    Módulo de muestreo de los datos de la DB en consola.

---
+++ TP1_IPP/Situacion2/situacion2Muestro.py
@@ -0,0 +1,42 @@
+# Importo la librería sqlite3
+import sqlite3
+
+# Conecto a la base de datos (Seria como el ruta a la base de datos)
+mi_conexion = sqlite3.connect('TP1_IPP\\Situacion2\\biblioteca.db')
+
+# Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
+mi_cursor = mi_conexion.cursor()
+
+# Selecciono datos de la tabla 'CONTACTOS'
+mi_cursor.execute("SELECT * FROM LIBROS")
+
+# el metodo fetchall() devuelve la cantidad los registros (filas) de la consulta
+cantidad_registros = mi_cursor.fetchall()
+
+# Imprimo los datos obtenidos
+k = 0
+for registro in cantidad_registros:
+    print(f"\nLibro nro {k + 1}.\n- ID: {registro[0]}\n- TITULO: {registro[1]}\n- AUTOR: {registro[2]}\n- GENERO: {registro[3]}\n- ANO: {registro[4]}\n")
+    k = k + 1
+# TABLA ORDENADA
+input("Presione Enter para ver los registros ordenados por GENERO...")
+# Ordeno los registros por el campo 'GENERO'
+mi_cursor.execute("SELECT * FROM LIBROS ORDER BY GENERO")
+cantidad_registros_ordenados = mi_cursor.fetchall()    
+# Imprimo los datos obtenidos
+c = 0
+for registro in cantidad_registros_ordenados:
+    print(f"\nLibro nro {c + 1}.\n- ID: {registro[0]}\n- TITULO: {registro[1]}\n- AUTOR: {registro[2]}\n- GENERO: {registro[3]}\n- ANO: {registro[4]}\n")
+    c = c + 1
+
+input("Presione Enter para ver los registros ordenados por ANO...")
+# Ordeno los registros por el campo 'ANO'
+mi_cursor.execute("SELECT * FROM LIBROS ORDER BY ANO")
+cantidad_registros_ordenados = mi_cursor.fetchall()    
+# Imprimo los datos obtenidos
+i = 0
+for registro in cantidad_registros_ordenados:
+    print(f"\nLibro nro {i + 1}.\n- ID: {registro[0]}\n- TITULO: {registro[1]}\n- AUTOR: {registro[2]}\n- GENERO: {registro[3]}\n- ANO: {registro[4]}\n")
+    i = i + 1
+
+mi_conexion.close()  # Cierro la conexión a la base de datos
