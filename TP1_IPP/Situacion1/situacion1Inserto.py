9dfd6dcfdf293fe304b7480020e53c874a2db335
Mauro <mauro.presso@gmail.com>
2025-08-16 14:47:37 -0300

    Nueva versión de situacion1Inserto.py

--- TP1_IPP/Situacion1/situacion1Inserto.py
+++ TP1_IPP/Situacion1/situacion1Inserto.py
@@ -2,19 +2,23 @@
 import sqlite3
 
 # Conecto a la base de datos (Seria como el ruta a la base de datos)
-conexion = sqlite3.connect('empresa.db')
+mi_conexion = sqlite3.connect('TP1_IPP\\Situacion1\\empresa.db')
 
 # Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
-cursor = conexion.cursor()
+mi_cursor = mi_conexion.cursor()
 
 # Inserto datos en la tabla 'EMPLEADOS'
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (1, 'Juan', 'Perez', 'Administrativo', 1234567)")
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (2, 'Pepe', 'Sanchez', 'Informatico', 1834960)")
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (3, 'Dardo', 'Rodriguez', 'Vendedor', 1224559)")
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (4, 'Elena', 'Fuseneco', 'Limpieza', 934567)")
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (5, 'Edgardo', 'Beltran', 'Mantenimiento', 934570)")
-cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (6, 'Rosa', 'Juarez', 'Secretaria', 834567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (1, 'Juan', 'Perez', 'Administrativo', 1234567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (2, 'Pepe', 'Sanchez', 'Informatico', 1834960)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (3, 'Dardo', 'Rodriguez', 'Vendedor', 1224559)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (4, 'Elena', 'Fuseneco', 'Limpieza', 934567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (5, 'Edgardo', 'Beltran', 'Mantenimiento', 934570)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (6, 'Rosa', 'Juarez', 'Secretaria', 834567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (7, 'Miguel', 'Faermann', 'Supervisor', 984567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (8, 'Guillermo', 'Granados', 'Contratista', 854567)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (9, 'Alberto', 'Hernandez', 'Logística', 884579)")
+mi_cursor.execute("INSERT INTO EMPLEADOS (ID, NOMBRE, APELLIDO, PUESTO, SALARIO) VALUES (10, 'Maximo', 'Sartori', 'Instructor', 734560)")
 # Para que los cambios se guarden en la base de datos
-conexion.commit()
+mi_conexion.commit()
 
-conexion.close()  # Cierro la conexión a la base de datos
+mi_conexion.close()  # Cierro la conexión a la base de datos
