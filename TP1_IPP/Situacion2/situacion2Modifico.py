d700c27a8fd7264ded7eb8358b50fda0c95b706b
Mauro Presso <mauro.presso@gmail.com>
2025-08-19 09:41:10 -0300

    Modulo de modificacion de la situacion 2

---
+++ TP1_IPP/Situacion2/situacion2Modifico.py
@@ -0,0 +1,26 @@
+import os
+os.system('cls')
+
+# Importo la librería sqlite3
+import sqlite3
+
+# Conecto a la base de datos (Seria como el ruta a la base de datos)
+mi_conexion = sqlite3.connect('TP1_IPP\\Situacion2\\biblioteca.db')
+
+# Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
+mi_cursor = mi_conexion.cursor()
+
+# Selecciono datos de la tabla 'LIBROS'
+mi_cursor.execute("SELECT * FROM LIBROS")
+
+# Modifico datos en la tabla 'EMPLEADOS'
+mi_cursor.execute("UPDATE LIBROS SET ANO = 1945 WHERE TITULO = 'Los amorios de un procer'")
+mi_cursor.execute("UPDATE LIBROS SET AUTOR = 'Jose Alvarado' WHERE AUTOR = 'Pepe Sanchez'")
+mi_cursor.execute("UPDATE LIBROS SET TITULO = 'Furia Nocturna' WHERE GENERO = 'Terror'")
+mi_cursor.execute("UPDATE LIBROS SET TITULO = 'El reino de Melniboné' WHERE ANO = 2011")
+mi_cursor.execute("UPDATE LIBROS SET GENERO = 'Novela Juvenil' WHERE ID = 8")
+
+# Para que los cambios se guarden en la base de datos
+mi_conexion.commit()
+
+mi_conexion.close()  # Cierro la conexión a la base de datos
