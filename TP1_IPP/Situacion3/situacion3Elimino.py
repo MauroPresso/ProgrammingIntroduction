71afe7b62d0d213bef347bce2b2e24ff7cda1d01
Mauro Presso <mauro.presso@gmail.com>
2025-08-19 10:09:09 -0300

    Modulo de eliminacion de datos de la situacion 3

---
+++ TP1_IPP/Situacion3/situacion3Elimino.py
@@ -0,0 +1,23 @@
+import os
+os.system('cls')
+
+# Importo la librería sqlite3
+import sqlite3
+
+# Conecto a la base de datos (Seria como el ruta a la base de datos)
+mi_conexion = sqlite3.connect('TP1_IPP\\Situacion3\\tienda.db')
+
+# Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
+mi_cursor = mi_conexion.cursor()
+
+# Selecciono datos de la tabla 'PRODUCTOS'
+mi_cursor.execute("SELECT * FROM PRODUCTOS")
+
+# Elimino datos en la tabla 'EMPLEADOS'
+mi_cursor.execute("DELETE FROM PRODUCTOS WHERE PRECIO > 50000")
+mi_cursor.execute("DELETE FROM PRODUCTOS WHERE CATEGORIA = 'Higiene personal'")
+
+# Para que los cambios se guarden en la base de datos
+mi_conexion.commit()
+
+mi_conexion.close()  # Cierro la conexión a la base de datos
