a5828c7d1e1aa13150d012f6047c88e059fb8d96
Mauro <mauro.presso@gmail.com>
2025-08-19 20:27:42 -0300

    Probando para el mini test.

--- TP0_IPP/Basededatos1.py
+++ TP0_IPP/Basededatos1.py
@@ -8,11 +8,11 @@ conexion = sqlite3.connect('TP0_IPP\\basededatos0.db')
 cursor = conexion.cursor()
 
 # Inserto datos en la tabla 'CONTACTOS'
-cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Juan Perez', 123456789)")
-cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Maria Lopez', 987654321)")
-cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Carlos Gomez', 456789123)")
-cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Pepe Sanchez', 456789123)")
-
+#cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Juan Perez', 123456789)")
+#cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Maria Lopez', 987654321)")
+#cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Carlos Gomez', 456789123)")
+#cursor.execute("INSERT INTO CONTACTOS (NOMBRE, TELEFONO) VALUES ('Pepe Sanchez', 456789123)")
+cursor.execute("INSERT INTO CONTACTOS (NOMBRE) VALUES ('Rick Sanchez')")
 # Para que los cambios se guarden en la base de datos
 conexion.commit()
 
