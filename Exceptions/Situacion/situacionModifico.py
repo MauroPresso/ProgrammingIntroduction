e628f62b8560e12892199ce057df159b763674a6
Mauro <mauro.presso@gmail.com>
2025-08-19 19:40:14 -0300

    .

--- Exceptions/Situacion/situacionModifico.py
+++ Exceptions/Situacion/situacionModifico.py
@@ -4,23 +4,22 @@ os.system('cls')
 # Importo la librería sqlite3
 import sqlite3
 
-# Conecto a la base de datos (Seria como el ruta a la base de datos)
-mi_conexion = sqlite3.connect('TP1_IPP\\Situacion1\\empresa.db')
-
-# Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
-mi_cursor = mi_conexion.cursor()
-
-# Selecciono datos de la tabla 'EMPLEADOS'
-mi_cursor.execute("SELECT * FROM EMPLEADOS")
-
-# Modifico datos en la tabla 'EMPLEADOS'
-mi_cursor.execute("UPDATE EMPLEADOS SET SALARIO = 906357 WHERE APELLIDO = 'Sartori'")
-mi_cursor.execute("UPDATE EMPLEADOS SET NOMBRE = 'Jose' WHERE NOMBRE = 'Pepe'")
-mi_cursor.execute("UPDATE EMPLEADOS SET PUESTO = 'Transportista' WHERE PUESTO = 'Logística'")
-mi_cursor.execute("UPDATE EMPLEADOS SET SALARIO = 2105987 WHERE PUESTO = 'Informatico'")
-mi_cursor.execute("UPDATE EMPLEADOS SET APELLIDO = 'Faerman' WHERE APELLIDO = 'Faermann'")
-
-# Para que los cambios se guarden en la base de datos
-mi_conexion.commit()
-
-mi_conexion.close()  # Cierro la conexión a la base de datos
+try:
+    # Conecto a la base de datos (Seria como el ruta a la base de datos)
+    mi_conexion = sqlite3.connect('TP1_IPP\\Situacion\\empresa.db')
+    # Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
+    mi_cursor = mi_conexion.cursor()
+    # Selecciono datos de la tabla 'EMPLEADOS'
+    mi_cursor.execute("SELECT * FROM EMPLEADOS")
+    # Modifico datos en la tabla 'EMPLEADOS'
+    mi_cursor.execute("UPDATE EMPLEADOS SET SALARIO = 906357 WHERE APELLIDO = 'Sartori'")
+    mi_cursor.execute("UPDATE EMPLEADOS SET NOMBRE = 'Jose' WHERE NOMBRE = 'Pepe'")
+    mi_cursor.execute("UPDATE EMPLEADOS SET PUESTO = 'Transportista' WHERE PUESTO = 'Logística'")
+    mi_cursor.execute("UPDATE EMPLEADOS SET SALARIO = 2105987 WHERE PUESTO = 'Informatico'")
+    mi_cursor.execute("UPDATE EMPLEADOS SET APELLIDO = 'Faerman' WHERE APELLIDO = 'Faermann'")
+    # Para que los cambios se guarden en la base de datos
+    mi_conexion.commit()
+    # Cierro la conexión a la base de datos
+    mi_conexion.close()  
+except:
+    print("¡ERROR: Problemas con la base de datos de la empresa!\n¡Comuniquese con el administrador!\n¡Sepa disculpar los inconvenientes!\n¡Muchas gracias!")
