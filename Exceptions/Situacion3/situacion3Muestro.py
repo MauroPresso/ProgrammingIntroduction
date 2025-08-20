f91befe0cd09b54e49127bb4e4d16f6368717330
Mauro Presso <mauro.presso@gmail.com>
2025-08-20 20:03:01 -0300

    Todo ok

---
+++ Exceptions/Situacion3/situacion3Muestro.py
@@ -0,0 +1,39 @@
+# Importo la librería sqlite3
+import sqlite3
+
+try:
+    # Conecto a la base de datos (Seria como el ruta a la base de datos)
+    mi_conexion = sqlite3.connect('Exceptions\\Situacion3\\tienda.db')
+    # Creo un cursor para ejecutar comandos SQL (Seria como el puntero a la base de datos)
+    mi_cursor = mi_conexion.cursor()
+    # Selecciono datos de la tabla 'CONTACTOS'
+    mi_cursor.execute("SELECT * FROM PRODUCTOS")
+    # el metodo fetchall() devuelve la cantidad los registros de la consulta
+    cantidad_registros = mi_cursor.fetchall()
+    # Imprimo los datos obtenidos
+    k = 0
+    for registro in cantidad_registros:
+        print(f"\nEmpleado nro {k + 1}.\n- ID: {registro[0]}\n- Nombre: {registro[1]}\n- CATEGORIA: {registro[2]}\n- PRECIO: {registro[3]}\n- STOCK: {registro[4]}\n")
+        k = k + 1
+    # TABLA ORDENADA
+    input("Presione Enter para ver los registros ordenados por NOMBRE...")
+    # Ordeno los registros por el campo 'NOMBRE'
+    mi_cursor.execute("SELECT * FROM PRODUCTOS ORDER BY NOMBRE")
+    cantidad_registros_ordenados = mi_cursor.fetchall()    
+    # Imprimo los datos obtenidos
+    c = 0
+    for registro in cantidad_registros_ordenados:
+        print(f"\nEmpleado nro {c + 1}.\n- ID: {registro[0]}\n- Nombre: {registro[1]}\n- CATEGORIA: {registro[2]}\n- PRECIO: {registro[3]}\n- STOCK: {registro[4]}\n")
+        c = c + 1
+    input("Presione Enter para ver los registros ordenados por PRECIO...")
+    # Ordeno los registros por el campo 'NOMBRE'
+    mi_cursor.execute("SELECT * FROM PRODUCTOS ORDER BY PRECIO")
+    cantidad_registros_ordenados = mi_cursor.fetchall()    
+    # Imprimo los datos obtenidos
+    i = 0
+    for registro in cantidad_registros_ordenados:
+        print(f"\nEmpleado nro {i + 1}.\n- ID: {registro[0]}\n- Nombre: {registro[1]}\n- CATEGORIA: {registro[2]}\n- PRECIO: {registro[3]}\n- STOCK: {registro[4]}\n")
+        i = i + 1
+    mi_conexion.close()  # Cierro la conexión a la base de datos
+except:
+    print("¡ERROR: Problemas con la base de datos de la empresa!\n¡Comuniquese con el administrador!\n¡Sepa disculpar los inconvenientes!\n¡Muchas gracias!")
