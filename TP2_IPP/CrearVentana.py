adc4217be276e63d82481d587e655457441d6749
Mauro Presso <mauro.presso@gmail.com>
2025-08-25 20:59:24 -0300

    Comienzo de interfaz grafica

---
+++ TP2_IPP/CrearVentana.py
@@ -0,0 +1,11 @@
+# Importo TKinter
+from tkinter import *
+
+# Creo la raiz
+raiz = Tk()
+
+# Mantendo la ventana abierta
+raiz.mainloop()
+
+
+
---
+++ TP2_IPP/CrearVentanaOpciones.py
@@ -0,0 +1,11 @@
+# Importo TKinter
+from tkinter import *
+
+# Creo la raiz
+raiz = Tk()
+
+# Mantendo la ventana abierta
+raiz.iconbitmap('TP2_IPP\\CARPETA_DE_ICONOS_amp_PUNTEROS_20250825\\Book.ico')
+
+
+
