e10ca20660fb6fe95bf225b53f33b6807a1f5148
Mauro Presso <mauro.presso@gmail.com>
2025-08-26 18:13:21 -0300

    Completando lo que me falto de ayer

--- TP2_IPP/CrearVentanaOpciones.py
+++ TP2_IPP/CrearVentanaOpciones.py
@@ -4,8 +4,14 @@ from tkinter import *
 # Creo la raiz
 raiz = Tk()
 
-# Mantendo la ventana abierta
+# Icono
 raiz.iconbitmap('TP2_IPP\\CARPETA_DE_ICONOS_amp_PUNTEROS_20250825\\Book.ico')
+# Titulo
+raiz.title("Intro A la Programacion - Año 2025")
+# Cursor
+raiz.config(cursor="star")
 
+# Ciclo
+raiz.mainloop()
 
 
