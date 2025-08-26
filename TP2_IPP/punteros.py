963bfce1eaf09e942326b806a7a224787606d32c
Mauro Presso <mauro.presso@gmail.com>
2025-08-26 18:03:41 -0300

    Punteros

---
+++ TP2_IPP/punteros.py
@@ -0,0 +1,12 @@
+# Importo TKinter
+from tkinter import *
+
+# Creo la raiz
+root = Tk()
+
+# Set geometry
+root.geometry("200x530")
+
+# List of cursors.
+
+cursors = ["arrow", "circle", "clock", "cross", "dotbox", "exchange", "fleur", "heart", "man", "mouse", "pirate", "plus", "shuttle", "sizing", "spider", "spraycan", "star", "target", "tcross", "trek"]
