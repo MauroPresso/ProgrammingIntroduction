a871eddb7d917796e5f9338cea55f8787350353c
Mauro Presso <mauro.presso@gmail.com>
2025-10-05 16:13:29 -0300

    .

--- TP6_IPP/POO/ejercicio2POOcreacionClaseHeader.py
+++ TP6_IPP/POO/ejercicio2POOcreacionClaseHeader.py
@@ -5,15 +5,7 @@ con atributos como ID, tipo e información.
 """
 
 class Header():
-    def __init__(self, id, tipo, info):
+    def __init__(self, id=0, tipo="", info=""):
         self.id = id
         self.tipo = tipo
         self.info = info
-
-    # Otros métodos de la clase
-    def Agregar(self):
-        print(f"Se agrego el Header del dispositivo:\n-ID: {self.id}\n-Tipo: {self.tipo}\n-Información: {self.info}")
-    def Modificar(self):
-        print(f"Se modificó el Header del dispositivo:\n-ID: {self.id}\n-Tipo: {self.tipo}\n-Información: {self.info}")
-    def Eliminar(self):
-        print(f"Se eliminó el Header del dispositivo:\n-ID: {self.id}\n-Tipo: {self.tipo}\n-Información: {self.info}")
