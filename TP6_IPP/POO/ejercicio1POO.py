861daf4caf1dfd124ab80d9d9d30f85d38b35ec7
Mauro Presso <mauro.presso@gmail.com>
2025-10-05 14:34:28 -0300

    Se definen los metodos de la clase Alumno

--- TP6_IPP/POO/ejercicio1POO.py
+++ TP6_IPP/POO/ejercicio1POO.py
@@ -4,9 +4,17 @@ Este ejercicio consiste en crear una clase que represente un alumno, con atribut
 """
 
 class ALumno():
-    def __init__(self, id=0, nombre="", domicilio="", edad=0, dni=0): # Inicializador de la clase
+    # Constructor de la clase (me permite inicializar los atributos del objeto)
+    def __init__(self, id=0, nombre="", domicilio="", edad=0, dni=0): 
         self.id = id
         self.nombre = nombre
         self.domicilio = domicilio
         self.edad = edad
         self.dni = dni
+    # Otros métodos de la clase
+    def Agregar(self):
+        print(f"Se agrego el alumno:\n-Nombre: {self.nombre}\n-Edad: {self.edad}\n-DNI: {self.dni}")
+    def Modificar(self):
+        print(f"Se modificó el alumno:\n-Nombre: {self.nombre}\n-Edad: {self.edad}\n-DNI: {self.dni}")
+    def Eliminar(self):
+        print(f"Se eliminó el alumno:\n-Nombre: {self.nombre}\n-Edad: {self.edad}\n-DNI: {self.dni}")
