d0becd57b62b88ab7e0cfc16fe1653265528f6fd
Mauro Presso <mauro.presso@gmail.com>
2025-10-05 16:20:01 -0300

    Usar clase register

---
+++ TP6_IPP/POO/ejercicio2POOusarClaseRegister.py
@@ -0,0 +1,11 @@
+from ejercicio2POOcreacionClaseRegister import Register
+
+# Crear instancias de la clase Register
+register1 = Register(101, "Sensor", "Temperatura")
+register2 = Register(102, "Actuador", "Presión")
+
+# Usar los métodos de la clase Register
+register1.Agregar_Registro()
+register2.Modificar_Registro()
+register1.Eliminar_Registro()
+register2.Eliminar_Registro()
