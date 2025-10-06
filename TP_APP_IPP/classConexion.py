b674b375a919aad51bb7aa36b52ae844dd63e1b9
Mauro Presso <mauro.presso@gmail.com>
2025-10-06 20:34:38 -0300

    Creacion de la clase conexion.

---
+++ TP_APP_IPP/classConexion.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class Conexion:
+    def __init__(self):
+        self.miConexion = sqlite3.connect("TP_APP_IPP\\BDD_APP.db")
+        self.miCursor = self.miConexion.cursor()
+        self.miCursor.execute('''CREATE TABLE IF NOT EXISTS Alumnos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(50), domicilio VARCHAR(100), dni INTEGER, edad INTEGER)''')
+
+    def cerrar(self):
+        self.miConexion.commit()
+        self.miConexion.close()
