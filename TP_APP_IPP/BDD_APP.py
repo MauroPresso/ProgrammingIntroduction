deb0b18b440ec0d5704ebd84dc31c8fb51384ff7
Mauro Presso <mauro.presso@gmail.com>
2025-10-06 21:03:12 -0300

    Ahora si quedo como le quedo a Laura

--- TP_APP_IPP/BDD_APP.py
+++ TP_APP_IPP/BDD_APP.py
@@ -2,4 +2,7 @@ import sqlite3
 
 miConexion = sqlite3.connect("TP_APP_IPP\\BDD_APP.db")
 miCursor = miConexion.cursor()
-miCursor.execute('''CREATE TABLE Alumnos (id INTEGER NOT NULLPRIMARY KEY AUTOINCREMENT, nombre VARCHAR(50), domicilio VARCHAR(100), dni INTEGER, edad INTEGER)''')
+
+miCursor.execute('''CREATE TABLE IF NOT EXISTS Alumnos (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(50), domicilio VARCHAR(100), dni INTEGER, edad INTEGER)''')
+
+miConexion.close()
