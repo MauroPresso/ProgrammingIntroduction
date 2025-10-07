279e13b0ca2c716dd817b3b0f8c897aebe2840d0
Mauro Presso <mauro.presso@gmail.com>
2025-10-06 21:02:37 -0300

    Ahora si quedo como le quedo a Laura

--- TP_APP_IPP/classConexion.py
+++ TP_APP_IPP/classConexion.py
@@ -4,8 +4,6 @@ class Conexion:
     def __init__(self):
         self.miConexion = sqlite3.connect("TP_APP_IPP\\BDD_APP.db")
         self.miCursor = self.miConexion.cursor()
-        self.miCursor.execute('''CREATE TABLE Alumnos (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(50), domicilio VARCHAR(100), dni INTEGER, edad INTEGER)''')
-
     def cerrar(self):
         self.miConexion.commit()
         self.miConexion.close()
