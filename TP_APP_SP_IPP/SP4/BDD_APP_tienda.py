c7c560128a1b49299e3cc0f38b82aeb743cfda4c
Mauro Presso <mauro.presso@gmail.com>
2025-10-11 23:06:23 -0300

    .

---
+++ TP_APP_SP_IPP/SP4/BDD_APP_tienda.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+miConexion = sqlite3.connect("TP_APP_SP_IPP\\SP4\\BDD_APP_tienda.db")
+miCursor = miConexion.cursor()
+
+miCursor.execute('''CREATE TABLE IF NOT EXISTS TiendaOnline (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NombreDelCliente VARCHAR(50), Producto VARCHAR(100), FechaDeEntrega DATE, Categoria VARCHAR(50), ServiciosAdicionales INTEGER)''')
+
+miConexion.close()
