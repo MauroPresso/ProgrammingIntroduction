6e10f69b86969fe3718a14488ebd0367cffa0201
Mauro Presso <mauro.presso@gmail.com>
2025-10-11 22:39:23 -0300

    Clase Conexion

---
+++ TP_APP_SP_IPP/SP5/classConexion.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+class Conexion:
+    def __init__(self):
+        self.miConexion = sqlite3.connect("TP_APP_SP_IPP\\SP5\\BDD_APP_idiomas.db")
+        self.miCursor = self.miConexion.cursor()
+    def cerrar(self):
+        self.miConexion.commit() # COMMIT PREVIO AL CIERRE DE LA CONEXION
+        self.miConexion.close()
