1c8609fc70e515ff1f8f6c2669ebe27f88114ae6
Mauro Presso <mauro.presso@gmail.com>
2025-10-11 23:05:49 -0300

    .

---
+++ TP_APP_SP_IPP/SP4/classCompra.py
@@ -0,0 +1,46 @@
+import os
+os.system("cls")
+
+from datetime import date
+from tkinter import messagebox
+from classConexion import Conexion
+
+class Prestamo():  
+    # Constructor
+    def __init__(self, id=0, nombre="",titulo="", fecha=date.today(), categoria="", servicios=0):
+        self.id=id
+        self.nombre=nombre
+        self.titulo=titulo
+        self.fecha=fecha
+        self.categoria=categoria
+        self.servicios=servicios
+    
+    # Método para agregar un alumno
+    def Agregar(self):
+        conexBD=Conexion()
+        instruct_insert="INSERT INTO PrestamosDeLibros(NombreDelLector, Titulo, FechaDeDevolucion, Categoria, ServiciosAdicionales) VALUES ('%s', '%s', '%s', '%s', '%s')"
+        conexBD.miCursor.execute(instruct_insert % (self.nombre, self.titulo, self.fecha, self.categoria, self.servicios))
+        conexBD.miConexion.commit() # COMMIT DEL COMANDO INSERT
+        messagebox.showinfo("AGREGADO","Nuevo registro ingresado")
+        conexBD.cerrar()
+
+    # Método para modificar un alumno
+    def Modificar(self):
+        mensaje="""Se modifico el prestamo del lector %s
+        que tiene el libro %s
+        con fecha de devolucion %s
+        y categoria %s
+        y servicios adicionales %d
+        CON EXITO!!!""" %(self.nombre,self.titulo,self.fecha,self.categoria,self.servicios)
+        messagebox.showinfo("MODIFICAR",mensaje)
+
+    
+    # Método para eliminar un alumno
+    def Eliminar(self):
+        mensaje="""Se elimino el prestamo del lector %s
+        que tiene el libro %s
+        con fecha de devolucion %s
+        y categoria %s
+        y servicios adicionales %d
+        CON EXITO!!!""" %(self.nombre,self.titulo,self.fecha,self.categoria,self.servicios)
+        messagebox.showinfo("ELIMINAR",mensaje)
