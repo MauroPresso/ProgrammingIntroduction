99c77fb5d33f582925d8ccd4a822bd658eb94c05
Mauro Presso <mauro.presso@gmail.com>
2025-10-12 23:36:16 -0300

    .

--- TP_APP_SP_IPP/SP4/classCompra.py
+++ TP_APP_SP_IPP/SP4/classCompra.py
@@ -7,28 +7,28 @@ from classConexion import Conexion
 
 class Compra():  
     # Constructor
-    def __init__(self, id=0, nombre="", producto="", fecha=date.today(), horario=f"{00}:{00}:00", tipoDeCuenta="", contactos=0):
+    def __init__(self, id=0, nombre="", producto="", fecha=date.today(), horario=f"{00}:{00}:00", tipoDeCuenta="", preferencias=0):
         self.id=id
         self.nombre=nombre
         self.producto=producto
         self.fecha=fecha
         self.horario=horario
         self.tipoDeCuenta=tipoDeCuenta
-        self.contactos=contactos
+        self.preferencias=preferencias
 
     # Método para agregar un alumno
     def Agregar(self):
         conexBD=Conexion()
-        instruct_insert="INSERT INTO TurnosMedicos(NombreDelCliente, Producto, Fecha, Hora, TipoDeCuenta, Contactos) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
-        conexBD.miCursor.execute(instruct_insert % (self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.contactos))
+        instruct_insert="INSERT INTO TurnosMedicos(NombreDelCliente, Producto, Fecha, Hora, TipoDeCuenta, Preferencias) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
+        conexBD.miCursor.execute(instruct_insert % (self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.preferencias))
         conexBD.miConexion.commit() # COMMIT DEL COMANDO INSERT
         messagebox.showinfo("AGREGADO","Nuevo registro ingresado")
         conexBD.cerrar()
     # Método para modificar un alumno
     def Modificar(self):
-        mensaje="Se modifico la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y contactos %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.contactos)
+        mensaje="Se modifico la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y preferencias %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.preferencias)
         messagebox.showinfo("MODIFICAR",mensaje)
     # Método para eliminar un alumno
     def Eliminar(self):
-        mensaje="Se elimino la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y contactos %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.contactos)
+        mensaje="Se elimino la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y preferencias %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.preferencias)
         messagebox.showinfo("ELIMINAR",mensaje)
