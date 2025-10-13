924f0cd736093de5542985b8f27b900f29d81cd1
Mauro Presso <mauro.presso@gmail.com>
2025-10-12 23:12:21 -0300

    .

--- TP_APP_SP_IPP/SP4/BDD_APP_tienda.py
+++ TP_APP_SP_IPP/SP4/BDD_APP_tienda.py
@@ -3,6 +3,6 @@ import sqlite3
 miConexion = sqlite3.connect("TP_APP_SP_IPP\\SP4\\BDD_APP_tienda.db")
 miCursor = miConexion.cursor()
 
-miCursor.execute('''CREATE TABLE IF NOT EXISTS TiendaOnline (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NombreDelCliente VARCHAR(50), Producto VARCHAR(100), FechaDeEntregaAprox DATE, HorarioDeEntregaAprox TIME, TipoDeCuenta VARCHAR(50), Contactos INTEGER)''')
+miCursor.execute('''CREATE TABLE IF NOT EXISTS TiendaOnline (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NombreDelCliente VARCHAR(50), Producto VARCHAR(100), FechaDeEntregaAprox DATE, HorarioDeEntregaAprox TIME, TipoDeCuenta VARCHAR(50), Preferencias INTEGER)''')
 
 miConexion.close()
