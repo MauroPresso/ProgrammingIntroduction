c04d43effe5f61ed49acee16e4122e1c41e0c40f
Mauro Presso <mauro.presso@gmail.com>
2025-10-18 23:29:25 -0300

    Se agrego el metodo ListarCompras()

--- TP_APP_SP_IPP/SP4/classCompra.py
+++ TP_APP_SP_IPP/SP4/classCompra.py
@@ -16,7 +16,7 @@ class Compra():
         self.tipoDeCuenta=tipoDeCuenta
         self.preferencias=preferencias
 
-    # Método para agregar un alumno
+    # Método para agregar una compra
     def Agregar(self):
         conexBD=Conexion()
         instruct_insert="INSERT INTO TiendaOnline(NombreDelCliente, Producto, FechaDeEntregaAprox, HorarioDeEntregaAprox, TipoDeCuenta, Preferencias) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
@@ -24,11 +24,19 @@ class Compra():
         conexBD.miConexion.commit() # COMMIT DEL COMANDO INSERT
         messagebox.showinfo("AGREGADO","Nuevo registro ingresado")
         conexBD.cerrar()
-    # Método para modificar un alumno
+    # Método para modificar una compra
     def Modificar(self):
         mensaje="Se modifico la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y preferencias %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.preferencias)
         messagebox.showinfo("MODIFICAR",mensaje)
-    # Método para eliminar un alumno
+    # Método para eliminar una compra
     def Eliminar(self):
         mensaje="Se elimino la compra de %s que tiene el producto %s con fecha %s y horario %s y tipo de cuenta %s y preferencias %d CON EXITO!!!" %(self.nombre, self.producto, self.fecha, self.horario, self.tipoDeCuenta, self.preferencias)
         messagebox.showinfo("ELIMINAR",mensaje)
+    # Metodo para listar compras
+    def ListarCompras(): # no necesita self porque no usa atributos de instancia.
+        conexBD=Conexion() # crea el objeto de conexion
+        instruct_select="SELECT * FROM TiendaOnline ORDER BY id DESC" # ORDER BY id DESC para que muestre primero los mas viejos
+        conexBD.miCursor.execute(instruct_select) # ejecuta el SELECT
+        registros=conexBD.miCursor.fetchall() # fetchall() toma todos los registros del SELECT
+        conexBD.cerrar() # cierra la conexion
+        return registros # retorna los registros tomados por SELECT
