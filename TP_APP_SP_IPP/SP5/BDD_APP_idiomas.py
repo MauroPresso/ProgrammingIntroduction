66bdf7ab0d782dacae486c60c7b467ed83849054
Mauro Presso <mauro.presso@gmail.com>
2025-11-02 18:25:40 -0300

    Se agregaron campos para poner en la table de la BDD.

--- TP_APP_SP_IPP/SP5/BDD_APP_idiomas.py
+++ TP_APP_SP_IPP/SP5/BDD_APP_idiomas.py
@@ -3,6 +3,6 @@ import sqlite3
 miConexion = sqlite3.connect("TP_APP_SP_IPP\\SP5\\BDD_APP_idiomas.db")
 miCursor = miConexion.cursor()
 
-miCursor.execute('''CREATE TABLE IF NOT EXISTS EscuelaDeIdiomas (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NombreDelEstudiante VARCHAR(50), Idioma VARCHAR(100), FechaDeInscripcion DATE, Nivel VARCHAR(50), ServiciosAdicionales INTEGER)''')
+miCursor.execute('''CREATE TABLE IF NOT EXISTS EscuelaDeIdiomas (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NombreDelEstudiante VARCHAR(50), Idioma VARCHAR(100), FechaDeInscripcion DATE, Nivel VARCHAR(50), MaterialImpreso VARCHAR(3), ClasesGrabadas VARCHAR(3), TalleresExtras VARCHAR(3))''')
 
 miConexion.close()
