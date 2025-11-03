33ec5aeff200bfb034077f7510dd55828569c1a8
Mauro Presso <mauro.presso@gmail.com>
2025-11-03 19:27:42 -0300

    Creacion de la BDD

---
+++ PRESSO/DATOS.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+miConexion = sqlite3.connect("TP_APP_IPP\\BDD_APP.db")
+miCursor = miConexion.cursor()
+
+miCursor.execute('''CREATE TABLE IF NOT EXISTS Capacitacion (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nombre VARCHAR(50), Apellido VARCHAR(50), Email VARCHAR(50), Capacitacion VARCHAR(50), Modalidad VARCHAR(20))''')
+
+miConexion.close()
