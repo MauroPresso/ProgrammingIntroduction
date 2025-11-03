c4f0b0bccb4a5c4b00b9087ae169a22cc5977e9b
Mauro Presso <mauro.presso@gmail.com>
2025-11-03 19:30:30 -0300

    Nobre BDD

--- PRESSO/DATOS.py
+++ PRESSO/DATOS.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-miConexion = sqlite3.connect("TP_APP_IPP\\BDD_APP.db")
+miConexion = sqlite3.connect("DATOS.db")
 miCursor = miConexion.cursor()
 
 miCursor.execute('''CREATE TABLE IF NOT EXISTS Capacitacion (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nombre VARCHAR(50), Apellido VARCHAR(50), Email VARCHAR(50), Capacitacion VARCHAR(50), Modalidad VARCHAR(20))''')
